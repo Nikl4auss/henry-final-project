d5ef51f37749e19d2256d1a040f2e68017b929cd
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-07 18:54:20 -0300

    feature(models/Category) Added the model definition for Category

---
+++ api/src/models/Category.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true            
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }
+    })
+}
