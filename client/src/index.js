89318da092621c489d5af3b09ad6543dd110b389
gabrielweht <gabrielestebanw@gmail.com>
2022-07-06 21:49:52 -0300

    refactor(client/index.js): Tab correction

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,8 @@ import {store} from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>
 );
