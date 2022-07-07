69ae6fe04764a46354c6af5d7250be86db33ec97
catasan <santinicatalina@gmail.com>
2022-07-07 00:27:59 -0300

    SearchBar actions done, getProductsName

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const GET_PRODUCTS = 'GET_PRODUCTS'
+export const GET_PRODUCTS_NAME = 'GET_PRODUCTS_NAME'
 
 export function getProducts(){
     return async function(dispatch){
@@ -9,5 +10,19 @@ export function getProducts(){
             type: GET_PRODUCTS,
             payload: json.data
         })
+    }
+}
+
+export function getProductsName(name){
+    return async function (dispatch){
+        try{
+        var json = await axios.get(`http://localhost:3001/products?name=${name}`); //acá va la ruta de los productos
+        return dispatch({
+            type: GET_PRODUCTS_NAME,
+            payload: json.data //me devuelve lo que me devuela la ruta
+        })
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
