bd00d7ee471a30eacf04aef922ed5d5a76d30521
gabrielweht <gabrielestebanw@gmail.com>
2022-07-09 10:06:08 -0300

    filter actions and actions to bring the filters are added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,39 @@
 import axios from 'axios';
+import { BRANDS, CATEGORIES } from '../Variables Auxiliares/Auxiliar';
+import { GET_BRANDS, GET_CATEGORIES, GET_PRODUCTS } from './actions_types';
 
-export const GET_PRODUCTS = 'GET_PRODUCTS'
-
-export function getProducts(){
+export function getProducts(filters = {}){
+    let queryFilter = ''
+    if (filters.category && filters.brand){
+        if(filters.category.length > 0) {
+            queryFilter.length === 0 ? queryFilter = '?' : queryFilter = queryFilter + '&'
+            queryFilter = queryFilter + 'categories=' + filters.category.join('+')
+        }
+        if(filters.brand.length > 0) {
+            queryFilter.length === 0 ? queryFilter = '?' : queryFilter = queryFilter + '&'
+            queryFilter = queryFilter + 'brands=' + filters.brand.join('+')
+        }
+    }
     return async function(dispatch){
-        var json = await axios.get("https://v1-sneakers.p.rapidapi.com/v1/sneakers") //poner la ruta del BACK cuando esté lista
+        var json = await axios.get(`http://localhost:3001/products${queryFilter}`) 
         dispatch({
             type: GET_PRODUCTS,
-            payload: json.data
+            payload: json.data,
+            filters: filters
         })
+    }
+}
+
+export function getCategories (){
+    return {
+        type: GET_CATEGORIES,
+        payload: CATEGORIES
+    }
+}
+
+export function getBrands (){
+    return {
+        type: GET_BRANDS,
+        payload: BRANDS
     }
 }
