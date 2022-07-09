a3ad0008f7c98ef1c9663572995c8b15e1760efe
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-09 20:58:40 -0300

    feat(App/router) added route for the Product Detail component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import {BrowserRouter, Route} from 'react-router-dom';
 import {Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
-
+import ProductDetail from './components/ProductDetail/ProductDetail';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
       <Route exact path= '/home' element= {<Home />}/>
+      <Route path='/product'>
+        <Route path=':id' element= {<ProductDetail />}/>
+      </Route>
       </Routes>
     </div>
     </BrowserRouter>
