08db9c4b786f475588db9a9ca3d7adab8d386994
lrverges <lrverges@gmail.com>
2022-07-08 22:50:07 -0300

    add attribute image_logo

--- api/src/models/Brand.js
+++ api/src/models/Brand.js
@@ -1,16 +1,24 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('Brand', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        }
-    })
-}
+  sequelize.define(
+    "Brand",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      image_logo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    { timestamps: false }
+  );
+};
