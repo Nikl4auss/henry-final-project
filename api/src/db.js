c8119bc73166dc72e66654b2c00a5f26401acb2c
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-10 17:00:29 -0300

    feat(db) setted the relation between products and reviews

--- api/src/db.js
+++ api/src/db.js
@@ -26,7 +26,7 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Product, Category, Brand, Image_Product, Gender, Size, Stock, Store, Address, MainColor } = sequelize.models
+const { Product, Category, Brand, Image_Product, Gender, Size, Stock, Store, Address, MainColor, CustomerReview } = sequelize.models
 
 Brand.hasMany(Product)
 Product.belongsTo(Brand)
@@ -54,6 +54,11 @@ Product.belongsTo(Gender)
 
 Category.belongsToMany(Product, { through: 'Category_Products'})
 Product.belongsToMany(Category, { through: 'Category_Products'})
+
+Product.hasMany(CustomerReview, {as: 'reviews'})
+CustomerReview.belongsTo(Product, {as: 'product'})
+
+
 
 module.exports = {
     db: sequelize,
