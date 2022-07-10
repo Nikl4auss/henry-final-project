0d44497d28eefc40cf31fa098d1783bae9e97146
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-09 22:35:00 -0300

    feat(api/index.js) added the populateProducts to the index file to populate the db with products at server start

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const {PORT} = require('./src/utils/config')
 const server = require('./src/app.js');
 const {db} = require('./src/db');
-
-db.sync({ force: true }).then(() => {
+const { populateProducts } = require('./src/utils/products')
+db.sync({ force: true }).then( async () => {
     server.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
+        populateProducts()
     });
 })  
