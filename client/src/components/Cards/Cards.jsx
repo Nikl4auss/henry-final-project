3ca23e503bf18a53c817737f652a339915d01b15
gabrielweht <gabrielestebanw@gmail.com>
2022-07-10 11:23:18 -0300

    UseEffect is fixed so that it does not generate an infinite loop

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,24 +1,26 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getProducts } from "../../actions";
-import { PRODUCTS } from "../../AuxiliaryVariables/Auxiliar";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../actions";
 import Card from "../Card/Card";
 import styles from './cards.module.css'
 
 
 export default function Cards () {
-    // const products = useSelector(state => state.products)
-    // const filters = useSelector(state => state.filters)
-    // const name = useSelector(state => state.name)
-    // let dispatch = useDispatch()
+    console.log('Me estoy renderizando')
+    const products = useSelector(state => state.products)
+    const filters = useSelector(state => state.filters)
+    const name = useSelector(state => state.name)
+    let dispatch = useDispatch()
 
-    // useEffect(() => {
-    //     dispatch(getProducts(filters, name))
-    // }, [dispatch, filters, name])
+    useEffect(() => {
+        dispatch(getProducts(filters, name))
+    }, [dispatch])
+
+
 
     return(
         <div className={styles.divCards}>
-            {PRODUCTS.map(product => {
+            {products.map(product => {
                 return <Card 
                     id={product.id}
                     name={product.name}
