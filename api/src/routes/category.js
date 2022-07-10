d99868122a7030e8312a06e07717200be97e70a7
gabrielweht <gabrielestebanw@gmail.com>
2022-07-10 11:13:19 -0300

    Correction in category routes to search in DB

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -3,10 +3,9 @@ const {Category} = require("../db")
 
 const router = Router();
 
-router.get('/', async(req, res, next) => {
-    const name = req.query.name;
+router.get('/', async (req, res, next) => {
     try{
-        const categoryName= await Category.filter(d=>d.name.toLowerCase().includes(name.toLowerCase()))
+        const categoryName = await Category.findAll({})
         res.status(200).send(categoryName)
     }catch(err){
         next(err)
