779758c2bee5b8521b49168b38bee396e1e08381
catasan <santinicatalina@gmail.com>
2022-07-11 02:05:58 -0300

    Landing Page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Route} from 'react-router-dom';
 import {Routes} from 'react-router-dom';
 import Home from './components/Home/Home';
+import Landing from './components/LandingPage/Landing';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
+      <Route exact path= '/' element= {<Landing />}/>
       <Route exact path= '/home' element= {<Home />}/>
       <Route path='/product'>
         <Route path=':id' element= {<ProductDetail />}/>
---
+++ client/src/components/LandingPage/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import styles from './Landing.module.css';
+
+export default function Landing(){
+    return(
+            <div className={styles.landingImg}>
+                <h1 className= {styles.logo}>DAVO SHOES</h1>
+                <NavLink className={styles.navHome} to ='/home'>HOME</NavLink>
+            </div>
+    )
+}
