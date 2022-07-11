1566611f6fac5a3888d7895cf4af37c6d7f11050
gabrielweht <gabrielestebanw@gmail.com>
2022-07-11 19:46:35 -0300

    Added key prop

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ export default function Card ({ id, name, price, image}) {
 
     return (
         <Link to={`/product/${id}`}>
-            <div>
+            <div key={id}>
                 <img src={image} alt='Zapatillas'/>
                 <h3>{name}</h3>
                 <span>$ {price}</span>
