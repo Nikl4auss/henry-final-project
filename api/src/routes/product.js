8da6ad0b6e869e39afa6bf2ffe490df90c06bc8e
gabrielweht <gabrielestebanw@gmail.com>
2022-07-11 19:42:55 -0300

    Tab correction

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -60,43 +60,43 @@ router.get('/', async(req, res, next) => {
 
 router.post('/', async (req, res, next)=>{
     try{
-    const {
-       name,
-       description,
-       model,
-       price,
-       brand,
-       category,
-       image
-           } = req.body
-     const newProduct = await Product.create({     
+        const {
+            name,
+            description,
+            model,
+            price,
+            brand,
+            category,
+            image
+        } = req.body
+    const newProduct = await Product.create({     
         name,
         description,
         model,
         price
-       })
-const [dbBrand] = await Brand.findOrCreate({
-    where: {name: brand}
-})
-dbBrand.addProduct(newProduct)
-
-if(category){
-    for(i=0; i < category.length; i++){
-    const [dbCategory] = await Category.findOrCreate({
-        where: {name: category[i]}
     })
-    newProduct.addCategory(dbCategory)
-    }
-}
-
-if(image){
-    for(i=0; i < image.length; i++){
-    const [dbImage] = await Image_Product.findOrCreate({
-        where: {image: image[i]}
+    const [dbBrand] = await Brand.findOrCreate({
+        where: {name: brand}
     })
-    newProduct.addImage_Product(dbImage)
+    dbBrand.addProduct(newProduct)
+
+    if(category){
+        for(i=0; i < category.length; i++){
+            const [dbCategory] = await Category.findOrCreate({
+                where: {name: category[i]}
+            })
+            newProduct.addCategory(dbCategory)
+        }
     }
-}
+
+    if(image){
+        for(i=0; i < image.length; i++){
+            const [dbImage] = await Image_Product.findOrCreate({
+                where: {image: image[i]}
+            })
+            newProduct.addImage_Product(dbImage)
+        }
+    }
 
     res.send(newProduct)
     } catch (error){
