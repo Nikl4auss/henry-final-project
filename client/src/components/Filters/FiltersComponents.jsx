87d36c06f6a3371db1ea31f710907ccbb5188ef4
catasan <santinicatalina@gmail.com>
2022-07-12 18:48:17 -0300

    classname added

--- client/src/components/Filters/FiltersComponents.jsx
+++ client/src/components/Filters/FiltersComponents.jsx
@@ -1,26 +1,31 @@
 import styles from './filters.module.css'
 
 export function BrandsComponent ( {brands, filtersSelectedToRender, handleSelectBrand}) {
-    return (<div className={styles.filtersEach}>
-        <span className={styles.title}>Marca</span>
-        {brands.map((brand) => {
-            if(filtersSelectedToRender.includes(brand.name)) return undefined
-            return (
-                <button className={styles.btnFilters}
-                key={brand.id}
-                value={brand.name}
-                onClick={handleSelectBrand}>
-                    {brand.name}
-                </button>
-            )
-        })}
-    </div>)
+    return (
+        <>
+            <span className={styles.title}>Marca</span>
+            <div className={styles.filtersEach}>
+                {brands.map((brand) => {
+                    if(filtersSelectedToRender.includes(brand.name)) return undefined
+                    return (
+                        <button className={styles.btnFilters}
+                        key={brand.id}
+                        value={brand.name}
+                        onClick={handleSelectBrand}>
+                            {brand.name}
+                        </button>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
 
 export function CategoriesComponent ({ categories, filtersSelectedToRender, handleSelectCategory }) {
     return (
+        <>
+        <span className={styles.title}>Categoría</span>
         <div className={styles.filtersEach}>
-            <span className={styles.title}>Categoría</span>
             {categories.map((category) => {
                 if(filtersSelectedToRender.includes(category.name)) return undefined
                 return (
@@ -33,5 +38,7 @@ export function CategoriesComponent ({ categories, filtersSelectedToRender, hand
                 )
                 })}
         </div>
+        </>
     )
+
 }
