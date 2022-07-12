c9123e05d3b2cfbca245965ba5e69a0f06ba3200
alfonzomario <96298309+alfonzomario@users.noreply.github.com>
2022-07-12 01:35:04 -0300

    formulario listo sin la opcion otros

--- client/src/components/Admin/NewProduct.jsx
+++ client/src/components/Admin/NewProduct.jsx
@@ -1,43 +1,51 @@
-import React, {useState,useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBrands, getCategories } from '../../actions';
 import axios from 'axios';
 
-function postProduct(payload){
-    return async function (dispatch){
+function postProduct(payload) {
+    return async function (dispatch) {
         var json = await axios.post("http://localhost:3001/product", payload);
         return json;
     }
 }
 
-function validate(input){
-    let errors={};
-    if (!input.name){
-        errors.name = "Se necesita un nombre.";
-    } else if (!input.description){
-        errors.description = "Se necesita una descripción del producto.";
-    } else if (!input.price){
-        errors.price = "Se necesita asignarle un precio al producto.";
-    } else if (input.price < 0){
-        errors.price = "No está permitido un número negativo.";
-    } else if (!input.model){
-        errors.model = "Se necesita definir el modelo.";
-    } else if (!input.image[0]){
-        errors.image = "Se requiere la URL de la imagen del producto.";
-    } else if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!]))?/.test(input.image)){
-        errors.image = "La URL es inválida.";
-    } else if (!input.brands){
-        errors.brands = "Se requiere la marca del producto.";
-    } else if (!input.categories[0]){
-        errors.categories = "Una categoría es requerida.";
+function validate(input) {
+    let errors = {};
+    if (!input.name) {
+        errors.nombre = "Se necesita un nombre.";
+    } 
+    if (!input.description) {
+        errors.descripción = "Se necesita una descripción del producto.";
+    } 
+    if (!input.price) {
+        errors.precio = "Se necesita asignarle un precio al producto.";
+    } 
+    if (input.price < 0) {
+        errors.precio = "No está permitido un número negativo.";
+    } 
+    if (!input.model) {
+        errors.modelo = "Se necesita definir el modelo.";
+    } 
+    if (!input.image[0]) {
+        errors.imagen = "Se requiere la URL de la imagen del producto.";
+    } 
+    if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!]))?/.test(input.image)) {
+        errors.imagen = "La URL es inválida.";
+    } 
+    if (input.brands !== "") {
+        errors.marca = "Se requiere la marca del producto.";
+    } 
+    if (!input.categories[0]) {
+        errors.categorías = "Una categoría es requerida.";
     }
     return errors;
 };
-export default function NewProduct(){
+export default function NewProduct() {
     const dispatch = useDispatch()
-    const brands = useSelector((state)=>state.brands)
-    const categories = useSelector((state)=>state.categories)
+    const brands = useSelector((state) => state.brands)
+    const categories = useSelector((state) => state.categories)
     const [errors, setErrors] = useState({})
 
     const [input, setInput] = useState({
@@ -50,56 +58,56 @@ export default function NewProduct(){
         categories: []
     })
 
-    function handleChange(e){
+    function handleChange(e) {
         setInput({
             ...input,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
         setErrors(validate({
             ...input,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         }))
     }
 
-    function handleSelect(e){
-            setInput({
-                ...input,
-                image: [e.target.value],
-            })
+    function handleSelect(e) {
+        setInput({
+            ...input,
+            image: [e.target.value],
+        })
         setErrors(validate({
             ...input,
             image: [e.target.value],
         }))
     }
 
-    function handleSelect2(e){
+    function handleSelect2(e) {
         setInput({
             ...input,
             categories: [...input.categories, e.target.value]
         })
-    setErrors(validate({
-        ...input,
-        categories: [...input.categories, e.target.value]
-    }))
-}
+        setErrors(validate({
+            ...input,
+            categories: [...input.categories, e.target.value]
+        }))
+    }
 
-    function handleDelete2(e){
+    function handleDelete2(e) {
         setInput({
             ...input,
             categories: input.categories.filter(c => c !== e)
         })
     }
-    
-    function handleSubmit(e){
+
+    function handleSubmit(e) {
         e.preventDefault();
-        if(errors.name || errors.description || errors.price || errors.model || errors.image || errors.brands || errors.categories){
+        if (errors.nombre || errors.descripción || errors.precio || errors.modelo || errors.imagen || errors.marca || errors.categoría) {
             let sendErrors = [];
             for (const key in errors) {
-            sendErrors.push(`${key[0].toUpperCase()+key.slice(1)}: ${errors[key]}`)
+                sendErrors.push(`${key[0].toUpperCase() + key.slice(1)}: ${errors[key]}`)
             }
-            return alert(sendErrors) 
+            return alert(sendErrors)
         }
-        else if (input.name){
+        else if (input.name) {
             dispatch(postProduct(input))
             setInput({
                 name: "",
@@ -110,116 +118,95 @@ export default function NewProduct(){
                 brands: "",
                 categories: []
             })
-           return alert("Producto creado!")
+            return alert("Producto creado!")
         }
         return alert("Hace falta información!")
     }
 
-    useEffect(()=>{  
+    useEffect(() => {
         dispatch(getBrands())
         dispatch(getCategories())
-    },[dispatch]);
+    }, [dispatch]);
 
 
-    return(
+    return (
         <div>
             <div>
-            {input.categories.map(d=>
-                <div key={d}>
-                    <p>{d}</p>
-                    <button onClick={()=>handleDelete2(d)}>x</button>
-                </div>
+                <h1>Crear producto</h1>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <div>
+                        <label>Nombre:</label>
+                        <input
+                            type='text'
+                            value={input.name}
+                            name='name'
+                            onChange={(e) => handleChange(e)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Descripción:</label>
+                        <input
+                            type='text'
+                            value={input.description}
+                            name='description'
+                            onChange={(e) => handleChange(e)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Precio:</label>
+                        <input
+                            type='number'
+                            value={input.price}
+                            name='price'
+                            onChange={(e) => handleChange(e)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Modelo:</label>
+                        <input
+                            type='text'
+                            value={input.model}
+                            name='model'
+                            onChange={(e) => handleChange(e)}
+                        ></input>
+                    </div>
+                    <div>
+                        <label>Imagen:</label>
+                        <input
+                            type='text'
+                            value={input.image}
+                            name='image'
+                            onChange={(e) => handleSelect(e)}
+                        ></input>
+                    </div>
+                    <div>Marca:
+                        <select onChange={(e) => handleChange(e)}>
+                            <option value="" selected disabled hidden>Seleccione aquí:</option>
+                            {brands.map((m) => (
+                                <option key={m} value={m}>{m}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>Categoría:
+                        <select onChange={(e) => handleSelect2(e)}>
+                            <option value="" selected disabled hidden>Seleccione aquí:</option>
+                            {categories.map((c) => (
+                                <option key={c} value={c}>{c}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                {input.categories.map(d =>
+                    <div key={d}>
+                        <p>{d}</p>
+                        <button onClick={() => handleDelete2(d)}>x</button>
+                    </div>
                 )}
-           </div> 
-          <div>  
-            <h1>Crear producto</h1>
-            <form onSubmit={(e)=>handleSubmit(e)}>
-                <div>
-                    <label>Nombre:</label>
-                    <input
-                    type= 'text'
-                    value= {input.name}
-                    name= 'name'
-                    onChange={(e)=>handleChange(e)}
-                    ></input>
-                    {errors.name && (
-                        <p className='error'>{errors.name}</p>
-                    )}
-                </div>
-                <div>
-                <label>Descripción:</label>
-                    <input
-                    type= 'text'
-                    value= {input.description}
-                    name= 'description'
-                    onChange={(e)=>handleChange(e)}
-                    ></input>
-                    {errors.description && (
-                        <p className='error'>{errors.description}</p>
-                    )}
-                </div>
-                <div>
-                <label>Precio:</label>
-                    <input
-                    type= 'number'
-                    value= {input.price}
-                    name= 'price'
-                    onChange={(e)=>handleChange(e)}
-                    ></input>
-                    {errors.price && (
-                        <p className='error'>{errors.price}</p>
-                    )}
-                </div>
-                <div>
-                <label>Modelo:</label>
-                    <input
-                    type= 'text'
-                    value= {input.model}
-                    name= 'model'
-                    onChange={(e)=>handleChange(e)}
-                    ></input>
-                    {errors.model && (
-                        <p className='error'>{errors.model}</p>
-                    )}
-                </div>
-                <div>
-                <label>Imagen:</label>
-                    <input
-                    type= 'text'
-                    value= {input.image}
-                    name= 'image'
-                    onChange={(e)=>handleSelect(e)}
-                    ></input>
-                    {errors.image && (
-                        <p className='error'>{errors.image}</p>
-                    )}
-                </div>
-                <div>Marca:
-                <select onChange={(e)=>handleChange(e)}>
-                <option value="" selected disabled hidden>Seleccione aquí:</option>
-                    {brands.map((m)=>(
-                        <option key={m} value={m}>{m}</option>
-                   ))}
-                </select>
-                    {errors.brands && (
-                        <p className='error'>{errors.brands}</p>
-                    )}
-                </div>
-                <div>Categoría:
-                <select onChange={(e)=>handleSelect2(e)}>
-                <option value="" selected disabled hidden>Seleccione aquí:</option>
-                    {categories.map((c)=>(
-                        <option key={c} value={c}>{c}</option>
-                   ))}
-                </select>
-                    {errors.categories && (
-                        <p className='error'>{errors.categories}</p>
-                    )}
-                </div>
-                <button type='submit'>Crear</button>
-            </form>
-          </div>    
-         <Link to= '/home'><button>Volver</button></Link>
+            </div>
+                    <button type='submit'>Crear</button>
+                </form>
+            </div>
+            <Link to='/home'><button>Volver</button></Link>
         </div>
     )
 }
