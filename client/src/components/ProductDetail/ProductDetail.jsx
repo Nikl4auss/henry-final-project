9a79d4bdadc9dc0159f04c97033ae3851c1f8ac3
catasan <santinicatalina@gmail.com>
2022-07-12 18:51:46 -0300

    CSS on product detail card

--- client/src/components/ProductDetail/ProductDetail.jsx
+++ client/src/components/ProductDetail/ProductDetail.jsx
@@ -4,6 +4,7 @@ import { useParams, Link } from 'react-router-dom'
 
 import {product} from '../../AuxiliaryVariables/Auxiliar'
 import { getProduct } from '../../services/productsServices'
+import styles from './ProductDetail.module.css'
 
 function ProductDetail() {
   const { id } = useParams()
@@ -27,51 +28,57 @@ function ProductDetail() {
 
   return (
     <>
+    <Link to= '/home'>
+                <button className={styles.button}>Inicio</button>
+            </Link>
     {/* <div className='buttonHome' >
       <Link to='/Home' className='btn'>◀ Volver a Inicio</Link>
     </div> */}
+    
     <div>
     {productDetail.name ? (
+      <div className={styles.divContainer}>
       <div>
-      <div>
+      
         <div>
           {/* <ul>
             {productDetail.images?.map(({image, main}, inx) => {
               return <img key={inx} src={image} alt={productDetail.name} />
             })}
           </ul> */}
-          <img src={mainImage} alt="" />
+          <img className={styles.img} src={mainImage} alt="" />
         </div>        
       </div>
 
-      <div>
-        <h1>{productDetail.name}</h1>
-        <p>${productDetail.price}</p>
-        <select name="" id="">
+      <div className={styles.general}>
+        <h1 className={styles.name}>{productDetail.name}</h1>
+        <p className={styles.price}>${productDetail.price}</p>
+        <select className={styles.stock} name="" id="">
           {productDetail.Stocks?.map(({MainColor}, inx) => {
             if(MainColor?.name){
-              return <option key={inx} value={MainColor.name}>{MainColor.code}{MainColor.name}</option>
+              return <option  key={inx} value={MainColor.name}>{/*MainColor.code*/}{MainColor.name}</option>
             }
             return null
           })}
         </select>
-        <select name="" id="">
+        <select className={styles.stock} name="" id="">
           {productDetail.Stocks?.map(({Size}, inx) => {
             if(Size?.name){
-              return <option key={inx} value={Size.name}>{Size.name}</option>
+              return <option  key={inx} value={Size.name}>{Size.name}</option>
             }
             return null
           })}
         </select>
 
-        <button type='button'>Comprar</button>
-        <button type='button'>Agregar al Carrito</button>
+        <button className={styles.buy} type='button'>Comprar</button>
+        <button className={styles.add} type='button'>Agregar al Carrito</button>
       </div>
-      <div>
-        <p>{productDetail.description}</p>
-        <p>{productDetail.model}</p>
+      <div className={styles.general}>
+        <p className={styles.description}>{productDetail.description}</p>
+        <p className={styles.model}>{productDetail.model}</p>
       </div>
       </div>):<div>Cargando</div>}
+      
     </div>
     </>
   )
