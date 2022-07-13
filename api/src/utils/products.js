afbf876b03962e390d3bf575885d05962edce52f
gabrielweht <gabrielestebanw@gmail.com>
2022-07-13 08:26:09 -0300

    Deleted products Zapas

--- api/src/utils/products.js
+++ api/src/utils/products.js
@@ -13,118 +13,7 @@ const {
 const { ARRAYPRODUCT } = require("./arrayProduct");
 
 const PRODUCTS = [
-  {
-    id: 1,
-    name: "Zapas Piolas",
-    description: "Zapas de piolas de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://http2.mlstatic.com/D_NQ_NP_693424-MLA49985058216_052022-W.jpg",
-        main: true,
-      },
-    ],
-    price: 300.0,
-    brand: "Salomon",
-    model: "111TX",
-    categories: ["Zapas", "Piolas"],
-  },
-  {
-    id: 2,
-    name: "Zapas Trancas",
-    description: "Zapas de trancas de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e78cc288-65f2-4dca-99a5-4c06803f8312/air-force-1-lo-07-zapatillas-RRBKlk.png",
-        main: true,
-      },
-    ],
-    price: 400.0,
-    brand: "Topper",
-    model: "111TX",
-    categories: ["Moda", "Básicos"],
-  },
-  {
-    id: 3,
-    name: "Zapas Raras",
-    description: "Zapas de raras de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://d3ugyf2ht6aenh.cloudfront.net/stores/961/362/products/picsart_22-02-25_14-30-14-122-fbc9141bd92e0798c716458106031306-1024-1024.jpg",
-        main: true,
-      },
-    ],
-    price: 500.0,
-    brand: "Umbro",
-    model: "111TX",
-    categories: ["Moda", "Motorsport"],
-  },
-  {
-    id: 4,
-    name: "Zapas Facheras",
-    description: "Zapas de facheras de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://http2.mlstatic.com/D_NQ_NP_977082-MLA40388822119_012020-W.jpg",
-        main: true,
-      },
-    ],
-    price: 600.0,
-    brand: "Puma",
-    model: "111TX",
-    categories: ["Aerobics", "Fitness"],
-  },
-  {
-    id: 5,
-    name: "Zapas Omicrono",
-    description: "Zapas de ????? de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://s1.eestatic.com/2021/12/14/omicrono/software/634696947_218344871_1024x576.jpg",
-        main: true,
-      },
-    ],
-    price: 250.99,
-    brand: "Pony",
-    model: "111TX",
-    categories: ["Tenis", "Running"],
-  },
-  {
-    id: 6,
-    name: "Altas Llantas",
-    description: "Altas llantas de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFntL9jeQ2pTWyr2kOo0QzuaygQ00oJvF9g&usqp=CAU",
-        main: true,
-      },
-    ],
-    price: 1200.0,
-    brand: "Reebook",
-    model: "111TX",
-    categories: ["Moda", "Outdoor"],
-  },
-  {
-    id: 7,
-    name: "Altas Piolas",
-    description: "Altas piolas de alto rendimiento",
-    images: [
-      {
-        image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFntL9jeQ2pTWyr2kOo0QzuaygQ00oJvF9g&usqp=CAU",
-        main: true,
-      },
-    ],
-    price: 1200.0,
-    brand: "Reebook",
-    model: "111TX",
-    categories: ["Aerobics", "Training"],
-  },
+ 
 ];
 
 async function createStock(data) {
@@ -215,7 +104,7 @@ async function createProduct(data) {
 async function populateProducts() {
   try {
     await Promise.all(
-      PRODUCTS.map(async (product) => {
+      PRODUCTS?.map(async (product) => {
         await createProduct(product);
       })
     );
