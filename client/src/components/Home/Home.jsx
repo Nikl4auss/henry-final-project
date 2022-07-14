d7025c4448cc0b7ae1bac36910ab3175bf2bd0e0
gabrielweht <gabrielestebanw@gmail.com>
2022-07-14 16:29:20 -0300

    Replaced useState hook with useSessionStorage to maintain filters

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../actions";
 import NavBar from "../NavBar/NavBar";
@@ -7,17 +7,22 @@ import Filters from "../Filters/Filters";
 import Cards from "../Cards/Cards";
 import styles from './Home.module.css';
 import { Outlet } from "react-router-dom";
+import { useSessionStorage } from "../../services/useStorage";
 
 export default function Home(){
+    let nameSearched = useSelector(state => state.name)
+    let filtersSelected = useSelector(state => state.filtersSelected)
     const dispatch = useDispatch();
 
+    const [filtersToApply] = useSessionStorage('filtersSelected', filtersSelected)
+    const [ name ] = useSessionStorage('filtersSelected', nameSearched)
+
     useEffect(()=> {
-        dispatch(getProducts())
+        dispatch(getProducts(filtersToApply, name))
     }, [dispatch])
 
     return (
         <div className={styles.homeGrid}>
-
             <div className={styles.filtersContainer}>
             <Filters />
             </div>
