d0b5767013e9c0e2772cc02b44a4123d7b788f31
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-15 12:16:44 -0300

    created the LoginButton and LogoutButton components

---
+++ client/src/components/LoginButton/LoginButton.jsx
@@ -0,0 +1,11 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+
+function LoginButton() {
+    const { loginWithRedirect } = useAuth0()
+  return (
+    <button type='button' onClick={() => loginWithRedirect()}>Log In</button>
+  )
+}
+
+export default LoginButton
---
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+function LogoutButton() {
+
+  const { logout } = useAuth0()
+	
+  return (
+    <button type='button' onClick={() => logout({ returnTo: 'http://localhost:3000/home' })}>LogOut</button>
+  )
+}
+
+export default LogoutButton
