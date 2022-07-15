13698a12a518fb4fc64dfc25d87390cc5de45350
anmedina-arg <andres.medina.arg@gmail.com>
2022-07-15 18:39:54 -0300

    En el componente App.jsx agregué la ruta para que te lleve al carrito

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Landing from './components/LandingPage/Landing';
 import NavBar from './components/NavBar/NavBar';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import NewProduct from './components/Admin/NewProduct';
+import { ShopingCart } from './components/ShopCart/ShopingCart';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<NavBar/>}>
           <Route exact path= '/home' element= {<Home />}/>
           <Route exact path ='/nuevoproducto' element= {<NewProduct />}/>
+          <Route exact path = "/carrito" element = {<ShopingCart />} />
           <Route path='/product'>
             <Route path=':id' element= {<ProductDetail />}/>
           </Route>
