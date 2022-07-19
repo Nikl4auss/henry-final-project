2d4605c78dd74d21e9712b8c5614d5259b151b28
gabrielweht <gabrielestebanw@gmail.com>
2022-07-19 15:40:04 -0300

    route get modified

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res, next) => {
             },
             {
                 model: Stock,
-                attributes: ['stock_product'],
+                attributes: ['id', 'stock_product'],
                 include: [
                     {
                         model: MainColor,
