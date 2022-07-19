5dcdfe98a840badd04ee5b705ff2f0d22fae4cc7
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-19 16:22:21 -0300

    fixed bad config of the env variables

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,6 +1,6 @@
-export const DOMAIN = process.env.DOMAIN
-export const CLIENT_ID = process.env.CLIENT_ID
-export const REDIRECT_URI = process.env.REDIRECT_URI
-export const AUDIENCE = process.env.AUDIENCE
-export const API_URL = process.env.API_URL
+export const DOMAIN = process.env.REACT_APP_DOMAIN
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI
+export const AUDIENCE = process.env.REACT_APP_AUDIENCE
+export const API_URL = process.env.REACT_APP_API_URL
 
