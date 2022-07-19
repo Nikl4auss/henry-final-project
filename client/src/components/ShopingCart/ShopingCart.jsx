e92ea686258a17fd4c2046c516870939b8fdf47d
anmedina-arg <andres.medina.arg@gmail.com>
2022-07-19 14:37:19 -0300

    este es el contenedor de los items del carrito, tiene el boton pagar, falta terminar

--- client/src/components/ShopingCart/ShopingCart.jsx
+++ client/src/components/ShopingCart/ShopingCart.jsx
@@ -1,9 +1,45 @@
-import { ProductItem } from "./productItem"
+import { useLocalStorage } from "../../services/useStorage";
+import { ProductItem } from "./productItem";
+
+const producto = [{
+    id: 234,
+    stock_product: 3,
+    Size: {name: "11"},
+    MainColor: {name: "rojo"},
+    quantity: 1
+},
+{
+    id: 236,
+    stock_product: 3,
+    Size: {name: 111},
+    MainColor: {name: 8},
+    quantity: 2
+},
+{
+    id: 459,
+    stock_product: 5,
+    Size: {name: 111},
+    MainColor: {name: 5},
+    quantity: 3
+}]
 
 export function ShopingCart () {
+    const [cart] = useLocalStorage("cart")
+// (conidicion) ? (valor true) : (valor false)
     return (
         <div>
-            <ProductItem/>
+            <div>
+                { producto?.map((element) => <ProductItem 
+                                            id={element.id} 
+                                            color={element.MainColor.name} 
+                                            size={element.Size.name}
+                                            quantity={element.quantity}
+                                            stock={element.stock_product}
+                                            />)
+                }
+            </div>
+            <button>Pagar</button>
         </div>
     )
 }
+
