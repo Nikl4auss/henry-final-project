2013d4bf034a65baee8d65300bffb6110524742f
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-19 16:58:12 -0300

    modified the getProduct function to use the new axios instance for the api

--- client/src/services/productsServices.js
+++ client/src/services/productsServices.js
@@ -1,8 +1,22 @@
 import axios from 'axios'
+import {API_URL} from '../utils/config'
 
-const baseUrl = 'http://localhost:3001/'
+const apiInstance = axios.create({
+    baseURL: API_URL
+})
 
 export async function getProduct(id){
-    const {data} = await axios.get(`${baseUrl}product?id=${id}`)
+    // const {data} = await axios.get(`${baseUrl}product?id=${id}`)
+    const {data} = await apiInstance.get(`/product?id=${id}`)
+    return data
+}
+
+export async function createProduct(product, token){
+    const {data} = await apiInstance.post('/product', product, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
     return data
 }
