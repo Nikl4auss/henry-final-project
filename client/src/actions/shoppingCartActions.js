0198b49a2edae65cbee631e3b54edc2e36072486
anmedina-arg <andres.medina.arg@gmail.com>
2022-07-19 14:40:45 -0300

    se elimmina un archivo que no sirve

--- client/src/actions/shoppingCartActions.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TO_CART ="ADD_TO_CART";
-export const REMOVE_ONE_TO_CART ="REMOVE_ONE_TO_CART";
-export const REMOVE_ALL_TO_CART ="REMOVE_ALL_TO_CART";
-export const CLEAR_CART ="CLEAR_CART";
-export const TOTAL_AMOUNT_CART ="TOTAL_AMOUNT_CART";
