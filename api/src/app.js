00c505f3fe6418db889212fce44c17403d24ab38
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-18 21:03:09 -0300

    modified the app.js to use cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');;
+const cors = require('cors');
 const morgan = require('morgan');
 const routes = require('./routes');
 
@@ -9,17 +10,18 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors())
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
 server.use('/', routes);
 
