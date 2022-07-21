c8c102ab56dfbdf223474703dff45bcd8569a24c
gabrielweht <gabrielestebanw@gmail.com>
2022-07-21 19:33:38 -0300

    Resolve conflicts

--- client/src/components/Modal/ProductOptions.jsx
+++ client/src/components/Modal/ProductOptions.jsx
@@ -81,12 +81,14 @@ export default function ProductOptions({ active, setActive, stock, name, price})
     }
 
     async function addProductToCart() {
-            // try {
-            //     await axios.post(`http://localhost:3001/line_cart/${idStockSelected.id}?quantity=${quantity}`)
-            //     setActive(!active)
-            // } catch(err){
-            //     console.log(err)
-            // }
+         // if(isAuthenticated){
+        //     try {
+        //         await axios.post(`http://localhost:3001/line_cart/${idStockSelected.id}?quantity=${quantity}`)
+        //         setActive(!active)
+        //     } catch(err){
+        //         console.log(err)
+        //     }
+        // } else {
             let validateCart = cartLS.find(el => el.id === idStockSelected.id) 
             console.log(validateCart)
             if(validateCart) return;
