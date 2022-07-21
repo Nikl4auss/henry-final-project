221f735c2c66ef28bb46502b5535f06536ec97bb
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-20 21:23:11 -0300

    made the necesary changes to deploy the api to heroku

--- api/index.js
+++ api/index.js
@@ -3,13 +3,20 @@ const server = require('./src/app.js');
 const {db} = require('./src/db');
 const { populateProducts, populateProductsDos } = require('./src/utils/products');
 const loadDefaultValues = require('./src/utils/loadDefaultValues');
-
+const { NODE_ENV } = require('./src/utils/config')
 loadDefaultValues
-db.sync({ force: true }).then( async () => {
+db.sync({force: 
+    NODE_ENV === 'TEST' || NODE_ENV === 'DEVELOPMENT' 
+    ? true 
+    : false})
+    .then( async () => {
     server.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
-        populateProducts()
-        populateProductsDos();
-        loadDefaultValues();
+        if(NODE_ENV === "TEST" || NODE_ENV === 'DEVELOPMENT'){
+            console.log('you are here')
+            populateProducts()
+            populateProductsDos();
+            loadDefaultValues();
+        }
     });
 })  
