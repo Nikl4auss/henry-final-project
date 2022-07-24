4502f10baa1ab1a9ebcc4e784837bc1152ec0a75
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-23 23:15:55 -0300

    removed the create product button and fixed the logo link using a hardcoded localhost url

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,62 +1,65 @@
-import { Outlet, useNavigate } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
+import { Outlet, useNavigate, Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 // import { ShopingCart } from '../ShopingCart/ShopingCart';
-import styles from './NavBar.module.css'
-import LoginButton from '../LoginButton/LoginButton';
-import { useAuth0 } from '@auth0/auth0-react';
-import LogoutButton from '../LogoutButton/LogoutButton';
-
+import styles from "./NavBar.module.css";
+import LoginButton from "../LoginButton/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../LogoutButton/LogoutButton";
 
 //import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 //import {} from '@fortawesome/free-solid-svg-icons'
 
-
-
-
 export default function NavBar() {
-    const navigate = useNavigate();
-    const { user, isAuthenticated } = useAuth0();
-    function onClickbutton() {
-        navigate('/nuevoProducto')
-    }
+  const navigate = useNavigate();
+  const { user, isAuthenticated } = useAuth0();
 
-    function clickToShopingCart() {
-        navigate("/carrito")
-    }
+  function clickToShopingCart() {
+    navigate("/carrito");
+  }
 
-    function clickToShopingCart () {
-        navigate("/carrito")
-    }
+  function clickToShopingCart() {
+    navigate("/carrito");
+  }
 
-    function clickToHome () {
-        navigate("/home")
-    }
-    return(
-        <div>
-        <nav className={styles.navbarContainer}>
-            <div className={styles.divTop}>
-                <div className={styles.logo}>
-                <a href="http://localhost:3000/home">
-                    <img width="200" height="70" src="https://res.cloudinary.com/davoshoes/image/upload/v1658524699/LOGO/davo_shoes_1000_500_px_rxlpz2.png"/>
-                </a>
-                </div>
-                <SearchBar/>
-                <button className={styles.btnNav} onClick={clickToShopingCart}>Mi carrito</button>
-                {/* <p className={styles.envío}>Envío gratis en 24hs a partir de $10.000</p> */}
-            {isAuthenticated && <button className={styles.btnNav} onClick={onClickbutton}>Cargar Productos</button>}
-            {isAuthenticated 
-                ? (<div className={styles.userLoginLogout}>
-                    <div className={styles.userLogin}>
-                        <img src={user.picture} alt="profile" className={styles.pictureUser}/>
-                        <p>{user.name}</p>
-                    </div>
-                        <LogoutButton />
-                    </div>
-                )  
-                : <LoginButton />
-            }
+  function clickToHome() {
+    navigate("/home");
+  }
+  return (
+    <div>
+      <nav className={styles.navbarContainer}>
+        <div className={styles.divTop}>
+          <div className={styles.logo}>
+            <Link to="/home">
+              <img
+                width="200"
+                height="70"
+                src="https://res.cloudinary.com/davoshoes/image/upload/v1658524699/LOGO/davo_shoes_1000_500_px_rxlpz2.png"
+                alt="page logo"
+              />
+            </Link>
+          </div>
+          <SearchBar />
+          <button className={styles.btnNav} onClick={clickToShopingCart}>
+            Mi carrito
+          </button>
+          {/* <p className={styles.envío}>Envío gratis en 24hs a partir de $10.000</p> */}
+          {isAuthenticated ? (
+            <div className={styles.userLoginLogout}>
+              <div className={styles.userLogin}>
+                <img
+                  src={user.picture}
+                  alt="profile"
+                  className={styles.pictureUser}
+                />
+                <p>{user.name}</p>
+              </div>
+              <LogoutButton />
             </div>
-            {/* <div className= {styles.menuGral}>
+          ) : (
+            <LoginButton />
+          )}
+        </div>
+        {/* <div className= {styles.menuGral}>
             <ul className= {styles.menu}>
                 <li><button>Categorías</button></li>
                 <li><button>Género</button></li>
@@ -69,9 +72,8 @@ export default function NavBar() {
                 <li><button>Carrito</button></li>
             </ul>
             </div> */}
-
-            </nav>
-            <Outlet />
-        </div>
-    )
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
