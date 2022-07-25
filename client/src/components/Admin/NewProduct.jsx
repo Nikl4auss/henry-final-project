a11daa0eb4b0bfae4078119fa5d91c7f2417ac9c
gabrielweht <gabrielestebanw@gmail.com>
2022-07-25 19:47:09 -0300

    Deleted validate function to create new function in services

--- client/src/components/Admin/NewProduct.jsx
+++ client/src/components/Admin/NewProduct.jsx
@@ -7,6 +7,7 @@ import swal from 'sweetalert';
 import styles from './NewProduct.module.css'
 import { useAuth0 } from "@auth0/auth0-react";
 import { createProduct } from '../../services/productsServices'
+import validate from '../../services/validate';
 
 
 
@@ -22,34 +23,6 @@ function postProduct(payload, token) {
     }
 }
 
-function validate(input) {
-    let errors = {};
-    if (!input.name) {
-        errors.nombre = "Se necesita un nombre.";
-    }
-    if (!input.description) {
-        errors.descripción = "Se necesita una descripción del producto.";
-    }
-    if (!input.price) {
-        errors.precio = "Se necesita asignarle un precio al producto.";
-    }
-    if (input.price < 0) {
-        errors.precio = "No está permitido un número negativo.";
-    }
-    if (!input.model) {
-        errors.modelo = "Se necesita definir el modelo.";
-    }
-    if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!]))?/.test(input.image)) {
-        errors.imagen = "La URL es inválida.";
-    }
-    if (input.brand === "empty") {
-        errors.marca = "Se requiere la marca del producto.";
-    }
-    if (!input.category[0]) {
-        errors.categorías = "Una categoría es requerida.";
-    }
-    return errors;
-};
 export default function NewProduct() {
     const { isAuthenticated, getAccessTokenSilently } = useAuth0();
     const dispatch = useDispatch()
