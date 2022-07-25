277aea11b14a79bd33bf60b2545062a10bafaaa0
anmedina-arg <andres.medina.arg@gmail.com>
2022-07-25 17:33:33 -0300

    eliminado de archivos en action, reducer y store

---
+++ client/src/actions/actions_types.js
@@ -0,0 +1,12 @@
+export const GET_PRODUCTS = 'GET_PRODUCTS'
+export const GET_CATEGORIES = 'GET_CATEGORIES'
+export const GET_BRANDS = 'GET_BRANDS'
+export const GET_PRODUCTS_NAME = 'GET_PRODUCTS_NAME'
+export const ERROR = 'ERROR'
+//************* DESDE AQUI SE GENERAN ACTIONS PARA EL CARRITO ;)
+export const GET_STOCK = "GET_STOCK";
+export const ADD_TO_CART ="ADD_TO_CART";
+export const REMOVE_ONE_TO_CART ="REMOVE_ONE_TO_CART";
+export const REMOVE_ALL_TO_CART ="REMOVE_ALL_TO_CART";
+export const CLEAR_CART ="CLEAR_CART";
+export const TOTAL_AMOUNT_CART ="TOTAL_AMOUNT_CART";
