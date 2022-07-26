1fa67edb949a6b800c42a94beb37ebbbfd06943b
anmedina-arg <andres.medina.arg@gmail.com>
2022-07-26 10:48:32 -0300

    corrección crash carrito cuando el localStorage está vacío

--- client/src/components/ShopingCart/ShopingCart.jsx
+++ client/src/components/ShopingCart/ShopingCart.jsx
@@ -19,7 +19,7 @@ export function ShopingCart() {
         return count
     }, [order])
 
-    cart.forEach(product => {
+    cart?.forEach(product => {
         arrayOrder.push({
             stock_product: product.stock_product,
             id: product.id,
