26a987cd820edbf2ec2625f19d77ad392f0fbbe8
gabrielweht <gabrielestebanw@gmail.com>
2022-07-27 15:53:31 -0300

    Added route to search line orders

---
+++ api/src/routes/line_order.js
@@ -0,0 +1,68 @@
+const { Router } = require ('express')
+const {Order, User, Line_order, Stock, MainColor, Size} = require("../db.js")
+
+const router = Router();
+
+router.post('/', async (req, res, next) => {
+    const {
+        totalPrice,
+        status,
+        payment_status,
+        products
+    } = req.body
+    
+    console.log(req.body)
+
+    try{
+        const newOrder = await Order.create({
+            totalPrice,
+            status,
+            payment_status
+        })
+
+        products.forEach(async prod => {
+            const newLineOrder = await Line_order.create ({
+                quantity: prod.quantity
+            })
+
+            const productStock = await Stock.findByPk(prod.id_stock)
+
+            newLineOrder.setStock(productStock)
+
+            newOrder.addLine_order(newLineOrder)
+        })
+
+        res.send('El producto fue creado con éxito')
+
+    } catch (error){
+    console.log(error)
+    }
+});
+
+router.get('/', async (req, res, next) => {
+
+    try {
+        const response = await Line_order.findAll({
+            include: [{
+                model: Stock,
+                include: [
+                    {
+                        model: MainColor,
+                        attributes: ['name', 'code']
+                    },
+                    {
+                        model: Size,
+                        attributes: ['name']
+                    },
+
+                ]
+            }]
+        })
+    
+        res.send(response)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router;
