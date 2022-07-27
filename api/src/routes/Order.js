854ef8f4e5787815d5a1fb9d07d908a9813fd831
alfonzomario <96298309+alfonzomario@users.noreply.github.com>
2022-07-27 12:52:02 -0300

    get order by id

--- api/src/routes/Order.js
+++ api/src/routes/Order.js
@@ -5,7 +5,20 @@ const checkPermissions = require('../middleware/checkPermissions')
 const router = Router();
 
 
-
+router.get('/:id', async (req, res, next)=>{
+    const idOrder = req.params.id;
+    try{
+        if (idOrder) {
+            let totalOrders = await Order.findAll();
+            let orderById = await totalOrders.filter(o=> o.id == idOrder)
+            orderById.length ?
+            res.status(200).send(orderById) :
+            res.status(404).send('ID unavailable.')
+       }
+    } catch (error){
+        next(error)
+    } 
+});
 
 router.post('/', async (req, res, next) => {
     try {
