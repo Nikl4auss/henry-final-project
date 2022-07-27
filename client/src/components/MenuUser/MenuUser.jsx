cfa1c75f85f81d5e98791392ec2fdffb930a5414
anmedina-arg <andres.medina.arg@gmail.com>
2022-07-27 18:08:12 -0300

    creación componente menu de usuario y css básico del mismo

---
+++ client/src/components/MenuUser/MenuUser.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import LogoutButton from "../LogoutButton/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import styles from "../MenuUser/MenuUser.module.css";
+import { useNavigate } from "react-router-dom";
+
+function MenuUser () {
+    const { user } = useAuth0();
+    const navigate = useNavigate()
+    const [visible, setVisible] = useState(false)
+
+    function openCloseMenu (e) {
+        e.preventDefault()
+        console.log("este de aqui "+ visible)
+        setVisible(!visible)
+    }
+
+    function goToUserDashboard () {
+        navigate("/userDasboard")
+        setVisible(!visible)
+    }
+
+    function goToUserOrders () {
+        navigate("/userOrders")
+        setVisible(!visible)
+    }
+
+    return (
+        <div>
+           <p className={styles.btnNav} onClick={ openCloseMenu }>{user.name}</p>
+                <ul className={ visible ? styles.showMenu : styles.hideMenu }>
+                    <button className={styles.item} onClick={goToUserDashboard}>Mi perfil</button>
+                    <button className={styles.item} onClick={goToUserOrders}>Mis pedidos</button>
+                    <LogoutButton />
+                </ul>
+        </div>
+    )
+}
+
+export default MenuUser
