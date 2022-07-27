dac1d0423d1b18b1fcb7cf4a558c7a7af9d39423
gabrielweht <gabrielestebanw@gmail.com>
2022-07-27 15:48:19 -0300

    Added db assosiations

--- api/src/db.js
+++ api/src/db.js
@@ -20,7 +20,7 @@ const modelDefiners = [];
 fs.readdirSync(path.join(__dirname, '/models'))
     .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
     .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+        modelDefiners.push(require(path.join(__dirname, '/models', file)));
     });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -30,7 +30,24 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Product, Category, Brand, Image_Product, Gender, Size, Stock, Store, Address, MainColor, CustomerReview, User, Cart, Line_cart } = sequelize.models
+const {
+    Product,
+    Category,
+    Brand,
+    Image_Product,
+    Gender,
+    Size,
+    Stock,
+    Store,
+    Address,
+    MainColor,
+    CustomerReview,
+    User,
+    Cart,
+    Line_cart,
+    Line_order,
+    Order
+} = sequelize.models
 
 Brand.hasMany(Product)
 Product.belongsTo(Brand)
@@ -50,20 +67,20 @@ Stock.belongsTo(Product)
 Address.hasOne(Store)
 Store.belongsTo(Address)
 
-Product.hasMany(Image_Product, {as: 'images'});
-Image_Product.belongsTo(Product, {as: 'product'});
+Product.hasMany(Image_Product, { as: 'images' });
+Image_Product.belongsTo(Product, { as: 'product' });
 
 Gender.hasMany(Product)
 Product.belongsTo(Gender)
 
-Category.belongsToMany(Product, { through: 'Category_Products'})
-Product.belongsToMany(Category, { through: 'Category_Products'})
+Category.belongsToMany(Product, { through: 'Category_Products' })
+Product.belongsToMany(Category, { through: 'Category_Products' })
 
-Product.hasMany(CustomerReview, {as: 'reviews'})
-CustomerReview.belongsTo(Product, {as: 'product'})
+Product.hasMany(CustomerReview, { as: 'reviews' })
+CustomerReview.belongsTo(Product, { as: 'product' })
 
-User.hasMany(CustomerReview, {as: 'reviews'})
-CustomerReview.belongsTo(User, {as: 'user'})
+User.hasMany(CustomerReview, { as: 'reviews' })
+CustomerReview.belongsTo(User, { as: 'user' })
 
 User.hasOne(Cart)
 Cart.belongsTo(User)
@@ -73,6 +90,15 @@ Line_cart.belongsTo(Cart)
 
 Stock.hasMany(Line_cart)
 Line_cart.belongsTo(Stock)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+Order.hasMany(Line_order)
+Line_order.belongsTo(Order)
+
+Stock.hasOne(Line_order)
+Line_order.belongsTo(Stock)
 
 module.exports = {
     db: sequelize,
