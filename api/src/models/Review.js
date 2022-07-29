0fa771ab8ae0ce67a38686f93f590c756a3d27b3
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-07-29 19:26:23 -0300

    created the Review model

---
+++ api/src/models/Review.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('../db')
+
+module.exports = (sequelize) => {
+    return sequelize.define('Review', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+            
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            defaultValue: '',
+            validate: {
+                len: [0,255]
+            },
+            
+        },
+        score: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+                max: 5
+            }
+        },
+    })
+}
