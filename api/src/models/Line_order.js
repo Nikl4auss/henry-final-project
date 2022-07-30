709c4a4d931a672649321f4c2a54db8ce6ce67fb
lrverges <lrverges@gmail.com>
2022-07-30 17:32:16 -0300

    fix/line_order

--- api/src/models/Line_order.js
+++ api/src/models/Line_order.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-    sequelize.define('Line_order', {
+    sequelize.define('Line_Order', {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
