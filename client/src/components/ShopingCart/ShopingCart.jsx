236039b3958a387671de78685d29ecb90de21814
catasan <santinicatalina@gmail.com>
2022-07-30 20:03:08 -0300

    CSS on shopping cart and product item

--- client/src/components/ShopingCart/ShopingCart.jsx
+++ client/src/components/ShopingCart/ShopingCart.jsx
@@ -7,7 +7,10 @@ import ProductItem from "./productItem";
 import { useAuth0 } from "@auth0/auth0-react";
 import { payCart } from "../../services/shopingCart";
 import Shipping from "../Payment/Shipping";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import styles from './ShoppingCart.module.css';
+import { IoMdClose } from "react-icons/io";
+
 
 export function ShopingCart() {
     const navigate = useNavigate();
@@ -19,6 +22,12 @@ export function ShopingCart() {
     let total = useMemo(() => {
         let count = 0
         order.forEach(pr => count = count + (pr.unit_price * pr.quantity))
+        return count
+    }, [order])
+
+    let articulos = useMemo(() => {
+        let count = 0
+        order.forEach(pr => count = count + pr.quantity)
         return count
     }, [order])
 
@@ -64,8 +73,17 @@ export function ShopingCart() {
     }
 
     return (
-        <div>
-            <div>
+        <div className={styles.grid}>
+            <div className={styles.cartTitle}>
+                <h1 className={styles.title}>Tu carrito</h1>
+                <h1 className={styles.quantity}>({articulos} productos)</h1>
+            </div>
+            <div className={styles.divClose}>
+                <Link to= '/home'>
+                <button className={styles.buttonClose}><IoMdClose/></button>
+                </Link>
+        </div>
+            <div className={styles.cartProducts}>
                 {order?.map((element, i) => <ProductItem
                     key={i}
                     id={element.id}
@@ -76,14 +94,21 @@ export function ShopingCart() {
                 />)
                 }
             </div>
-            <div>
-                <h1>Total:</h1>
-                <h1>{total}</h1>
+            <div className={styles.orderInfo}>
+                <h1 className={styles.orderResumen}>Resumen del pedido</h1>
+                    <h1 className={styles.cartSubtotal}>Subtotal:</h1>
+                <div className={styles.cartTotal}>
+                    <h1 className={styles.cartTotalTitle}>Total:</h1>
+                    <h1 className={styles.cartTotalPrice}>${total}.00</h1>
+                </div>
+                <div className={styles.btnContainer}>
+                <button onClick={redirectToPay} className={styles.buyBtn} >COMPRAR</button>
+                </div>
+                <br />
+                <div className={styles.btnContainer}>
+                    <button onClick={clearCart} className={styles.deleteCart}>Borrar el carrito</button>
+                </div>
             </div>
-            <button onClick={redirectToPay}>Pagar</button>
-            <br />
-
-            <button onClick={clearCart}>Borrar todo</button>
         </div>
     )
 }
--- client/src/components/ShopingCart/productItem.jsx
+++ client/src/components/ShopingCart/productItem.jsx
@@ -5,6 +5,7 @@ import { useLocalStorage } from "../../services/useStorage"
 import styles from "./productItem.module.css"
 import { setOrder } from "../../redux/actions"
 import {API_URL} from '../../utils/config.js'
+import { BsChevronUp, BsChevronDown } from "react-icons/bs";
 
 function ProductItem({ id, price, quantity, stock, name }) {
     let dispatch = useDispatch()
@@ -78,24 +79,35 @@ function ProductItem({ id, price, quantity, stock, name }) {
         dispatch(setOrder(orderFilter))
     }
 
-    return (<div>{
-        product?.id ?
-            <div className={styles.cardCart}>
-                <h3>{product?.Product.name}</h3>
-                <h3>${product?.Product.price}</h3>
-                <div className={styles.conteinerQuantity}>
-                    <h3>Cantidad: {cantidad}</h3>
-                    <div className={styles.containerBttn}>
-                        <button className={styles.button} onClick={oneMore}>+</button>
-                        <button className={styles.button} onClick={oneLess}>-</button>
+    return (
+    <div className={styles.cardGrid}>
+        {
+            product?.id ?
+                <div className={styles.cardCart}>
+                    <div className={styles.line1}>
+                        <h3 className={styles.cardName}>{product?.Product.name}</h3>
+                        {/* <div>{product?.Image_Product.image}</div> */}
+                        <h3 className={styles.cardPrice}>${product?.Product.price}</h3>
+                    </div>
+                    
+                    <div className={styles.line2}>
+                        <div className={styles.conteinerQuantity}>
+                            <h3>Cantidad: {cantidad}</h3>
+                            <div className={styles.containerBttn}>
+                            <button className={styles.button} onClick={oneMore}><BsChevronUp /></button>
+                            <button className={styles.button} onClick={oneLess}><BsChevronDown /></button>
+                        </div>
+                    </div>
+                    <div className={styles.cardSubtotal}>Subtotal:  ${product?.Product.price * cantidad}.00</div>
+                    </div>
+                    <div className={styles.cardDeleteCont}>
+                        <button className={styles.cardDelete}
+                        onClick={(e) => productDeleted(e)}>Eliminar producto</button>
                     </div>
                 </div>
-                <div>Subtotal: ${product?.Product.price * cantidad}.00</div>
-                <button
-                    onClick={(e) => productDeleted(e)}>Eliminar producto</button>
-            </div>
-            : <div>loading</div>
-    }</div>
+                : <div>loading</div>
+        }
+    </div>
     )
 }
 
