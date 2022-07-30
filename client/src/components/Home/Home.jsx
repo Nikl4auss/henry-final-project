7c779de1b83b8ddfc254d8d3fa1df07db8f2ee5f
gabrielweht <gabrielestebanw@gmail.com>
2022-07-30 13:21:30 -0300

    deleted console.log

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../../redux/actions";
+import { getCart, getProducts } from "../../redux/actions";
 import NavBar from "../NavBar/NavBar";
 import Filters from "../Filters/Filters";
 import Cards from "../Cards/Cards";
 import styles from './Home.module.css';
 import Paginado from "../Paginado/Paginado";
+import { useLocalStorage } from "../../services/useStorage";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 export default function Home() {
     let products = useSelector(state => state.products)
+    const [ cart ] = useLocalStorage('cart')
+    
+    let dispatch = useDispatch()
+    const { loginWithRedirect, isAuthenticated } = useAuth0()
+
+    useEffect(() => {
+        dispatch(getCart('5s5f5s5s'))
+    }, [dispatch])
 
     return (
         <div className={styles.homeGrid}>
