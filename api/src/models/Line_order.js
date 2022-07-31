ef37a44e7100438b6fb6c5994a016f1b3d9df072
gabrielweht <gabrielestebanw@gmail.com>
2022-07-31 17:32:28 -0300

    Added model Lineorder

---
+++ api/src/models/Line_order.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+
+    sequelize.define('Line_order', {
+
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    },
+    { timestamps: false }
+    )
+}
