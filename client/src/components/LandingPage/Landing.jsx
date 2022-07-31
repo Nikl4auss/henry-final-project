6e58fc17fb569b92826ca5e3b6b7a11dc42bd1a1
alfonzomario <96298309+alfonzomario@users.noreply.github.com>
2022-07-31 16:02:39 -0300

    inicio de sesion en landing

--- client/src/components/LandingPage/Landing.jsx
+++ client/src/components/LandingPage/Landing.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Landing.module.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
-export default function Landing(){
-    return(
-            <div className={styles.landingImg}>
-                <div className={styles.logo}>
+export default function Landing() {
+    const { user, isAuthenticated } = useAuth0();
+    const { loginWithRedirect, logout } = useAuth0()
+
+
+
+    return (
+        <div className={styles.landingImg}>
+            <div className={styles.logo}>
                 <a href="http://localhost:3000/home">
-                    <img width="400" height="140" src="https://res.cloudinary.com/davoshoes/image/upload/v1658524699/LOGO/davo_shoes_1000_500_px_rxlpz2.png"/>
+                    <img width="400" height="140" src="https://res.cloudinary.com/davoshoes/image/upload/v1658524699/LOGO/davo_shoes_1000_500_px_rxlpz2.png" />
                 </a>
-                </div>                <NavLink className={styles.navHome} to ='/home'>INICIO</NavLink>
             </div>
+            {isAuthenticated ? 
+            (<><button type='button' className={styles.ingresa} onClick={() => logout()}>CERRAR SESIÓN</button>
+            <NavLink className={styles.invitado} to='/home'>INICIO</NavLink></>)
+           : 
+           (<><button type='button' className={styles.ingresa} onClick={() => loginWithRedirect()}>INICIAR SESIÓN</button>
+           <NavLink className={styles.invitado} to='/home'>INVITADO</NavLink></>)
+            }
+        </div>
     )
 }
