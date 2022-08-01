69e5f2d210e03a696603a2d4fc30d0a84f67eaaa
Estefania Antonela Bologna <ebologna@italianolaplata.org.ar>
2022-08-01 12:05:40 -0300

    agregue footer y cambie el paginado abajo

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/actions";
 import NavBar from "../NavBar/NavBar";
 import Filters from "../Filters/Filters";
+import Footer from "../Footer/Footer";
 import Cards from "../Cards/Cards";
 import styles from './Home.module.css';
 import Paginado from "../Paginado/Paginado";
@@ -17,11 +18,14 @@ export default function Home() {
             <div className={styles.filtersContainer}>
                 <Filters />
             </div>
-            <div>
-                <Paginado />
+            <div>                
                 <div className={styles.cardsContainer}>
                     {products.length === 0 ? 'Cargando...' : <Cards />}
                 </div>
+                <Paginado />
+            </div>
+            <div className={styles.footerContainer}>
+                <Footer />
             </div>
         </div>
     )
