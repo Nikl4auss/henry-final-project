a51f6e2c0c9317179ad74ea9a0c2639deb3073e3
gabrielweht <gabrielestebanw@gmail.com>
2022-08-01 22:15:46 -0300

    últimos cambios

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,68 +1,65 @@
-const { Router } = require ('express')
-const {Order, User, Line_order, Stock, MainColor, Size, Image_Product, Product} = require("../db.js");
+const { Router } = require('express')
+const { Order, User, Line_order, Stock, MainColor, Size, Image_Product, Product } = require("../db.js");
 const checkJwt = require('../middleware/checkJwt')
 const checkPermissions = require('../middleware/checkPermissions')
 
 const router = Router();
 
-router.get('/:id', async(req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     const { id } = req.params
-    try{
-            const response = await Order.findOne({
-                where: {
-                    id: id
-                }, 
-                include: [
-                    {
-                        model: Line_order,
-                        include: [{
-                            model: Stock,
-                            include: [
-                                {
-                                    model: MainColor,
-                                    attributes: ['name', 'code']
-                                },
-                                {
-                                    model: Size,
-                                    attributes: ['name']
-                                },
-                                {
-                                    model: Product,
-                                    include: [{
-                                        model: Image_Product,
-                                        as: 'images'
-                                    }]
-                                },
-                            ]
-                        },{
-                            model: User,
-                            attributes: ['name', 'surname', 'email']
-                        }
-                    ]
+    try {
+        const response = await Order.findOne({
+            where: {
+                id: id
+            },
+            include: [
+                {
+                    model: Line_order,
+                    include: [{
+                        model: Stock,
+                        include: [
+                            {
+                                model: MainColor,
+                                attributes: ['name', 'code']
+                            },
+                            {
+                                model: Size,
+                                attributes: ['name']
+                            },
+                            {
+                                model: Product,
+                                include: [{
+                                    model: Image_Product,
+                                    as: 'images'
+                                }]
+                            },
+                        ]
                     }
-                ]
-            })
-            res.json(response)
-            
-    }catch(err){
+                    ]
+                }
+            ]
+        })
+        res.json(response)
+
+    } catch (err) {
         next(err)
     }
 });
 
 
-router.put('/:id', async (req, res, next)=>{
-    const {status} = req.body
+router.put('/:id', async (req, res, next) => {
+    const { status } = req.body
     const idOrder = req.params.id
     try {
         const orderById = Order.findOne({
-            where:{
+            where: {
                 id: idOrder
             }
         })
         const orderUpdated = await orderById.update({
             payment_status: status
         })
-        res.send({msg:'orden actualizada'})
+        res.send({ msg: 'orden actualizada' })
     } catch (error) {
         next(error)
     }
--- client/src/components/Admin/Orders/Orders.jsx
+++ client/src/components/Admin/Orders/Orders.jsx
@@ -2,39 +2,76 @@ import React, { useEffect } from "react"
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { getOrders } from "../../../services/productsServices"
+import { useSessionStorage } from "../../../services/useStorage"
 import styles from './Orders.module.css'
 
 
 export default function Orders() {
-    const [ orders, setOrders ] = useState([])
+    const [orders, setOrders] = useState([])
+    const [filterOrders, setFilterOrders] = useSessionStorage('filterOrder', 'empty')
+    const [filterPayment, setFilterPayment ] = useSessionStorage('filterPayment', 'empty')
 
     useEffect(() => {
-        getOrders().then(data => setOrders(data))
+        getOrders(filterOrders, filterPayment).then(data => setOrders(data))
     }, [])
 
+    function handleSelect(e) {
+        e.preventDefault()
+        getOrders(e.target.value, filterPayment).then(data => setOrders(data))
+        setFilterOrders(e.target.value)
+    }
+
+    function handleSelectPayment(e) {
+        e.preventDefault()
+        getOrders(filterOrders, e.target.value).then(data => setOrders(data))
+        setFilterPayment(e.target.value)
+    }
+
+
     return (
-        orders?.length > 0 ?
-            orders.map(order => {
-                return (
-                    <div className={styles.container}>
-                        <div className={styles.interContainer}>
-                            <div className={styles.divPrice}>
-                                <h2>Orden N°: {order.id}</h2>
-                                <h4>Total: ${order.totalPrice}</h4>
-                            </div>
-                            <div className={styles.divPrice}>
-                                <span>Entrega: {order.status}</span>
-                                <span>Estado de pago: {order.payment_status}</span>
-                            </div>
-                                <NavLink 
+        <div className={styles.allContainer}>
+            <div>
+                <select name='select' defaultValue={filterOrders} onChange={handleSelect}>
+                    <option value='empty' disabled hidden>Estado de entrega</option>
+                    <option value='Todos'>Todos</option>
+                    <option value='Pendiente'>Pendiente</option>
+                    <option value='Despachado'>Despachado</option>
+                    <option value='Entregado'>Entregado</option>
+                    <option value='Cancelado'>Cancelado</option>
+                    <option value='Devuelto'>Devuelto</option>
+                </select>
+                <select name='select' defaultValue={filterPayment} onChange={handleSelectPayment}>
+                    <option value='empty' disabled hidden>Estado de pago</option>
+                    <option value='Todos'>Todos</option>
+                    <option value='approved'>Aprobado</option>
+                    <option value='pending'>Pendiete</option>
+                    <option value='failure'>Fallado</option>
+                </select>
+            </div>
+            {orders?.length > 0 ?
+                orders.map(order => {
+                    return (
+                        <div className={styles.container}>
+                            <div className={styles.interContainer}>
+                                <div className={styles.divPrice}>
+                                    <h2>Orden N°: {order.id}</h2>
+                                    <h4>Total: ${order.totalPrice}</h4>
+                                </div>
+                                <div className={styles.divPrice}>
+                                    <span>Estado de entrega: {order.status}</span>
+                                    <span>Estado de pago: {order.payment_status}</span>
+                                </div>
+                                <NavLink
                                     className={styles.link}
                                     to={`${order.id}`}
                                 >
                                     Ver orden
                                 </NavLink>
+                            </div>
                         </div>
-                    </div>
-                )
-            }) : <div className={styles.loading}>Cargando...</div>
+                    )
+                })
+                : <div className={styles.loading}>Cargando...</div>}
+        </div>
     )
 }
