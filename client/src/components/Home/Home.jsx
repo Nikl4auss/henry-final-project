4a2f60c35783902b95f5ad71dba121960090802e
Nicolas Joel Marinelli <65145528+Nikl4auss@users.noreply.github.com>
2022-08-04 19:53:37 -0300

    MFK NUMBER

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
     useEffect(() => {
         if (isAuthenticated) {
             dispatch(getCart(user.sub))
-            if (cart.length >) {
+            if (cart.length > 0) {
                 cart.forEach(async pr => {
                     await apiInstance.post(`/line_cart/${pr.id}`, {
                         id_Cart: user.sub,
