cf7cc2a1fb583da955253946f75960ad173e2ae6
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2022-08-03 22:51:56 -0300

    erased unused function

--- api/index.js
+++ api/index.js
@@ -1,39 +1,38 @@
-const { PORT , NODE_ENV} = require('./src/utils/config')
+const { PORT, NODE_ENV } = require('./src/utils/config')
 const server = require('./src/app.js');
-const {db, Product} = require('./src/db');
+const { db, Product } = require('./src/db');
 const { populateProducts, populateProductsDos } = require('./src/utils/products');
 const loadDefaultValues = require('./src/utils/loadDefaultValues');
 const populateUsers = require('./src/utils/populateUsers');
-const { createUsers } = require("./src/utils/createUsers")
 
 
-if(NODE_ENV === 'PRODUCTION'){
-    db.sync().then(async () => {
-        await server.listen(PORT, async () => {
-            console.log(`Server running on port ${PORT}`);
-                const products = await Product.findAll();
-                if(!products.length){
-                    console.log('No hay productos en la base de datos, se cargaran los productos por defecto');
-                    populateProducts();
-                    populateProductsDos();
-                    loadDefaultValues();
-                    populateUsers()
-                    // createUsers()
-                }    
+if (NODE_ENV === 'PRODUCTION') {
+  db.sync().then(async () => {
+    await server.listen(PORT, async () => {
+      console.log(`Server running on port ${PORT}`);
+      const products = await Product.findAll();
+      if (!products.length) {
+        console.log('No hay productos en la base de datos, se cargaran los productos por defecto');
+        populateProducts();
+        populateProductsDos();
+        loadDefaultValues();
+        populateUsers()
+        // createUsers()
+      }
 
-        })
     })
+  })
 }
 else {
-    db.sync({ force: true })
-        .then( async () => {
-        server.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}`);
-            populateProducts()
-            populateProductsDos()
-            loadDefaultValues()
-            populateUsers()
-            // createUsers()
-        });
-    })  
+  db.sync({ force: true })
+    .then(async () => {
+      server.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+        populateProducts()
+        populateProductsDos()
+        loadDefaultValues()
+        populateUsers()
+        // createUsers()
+      });
+    })
 }
