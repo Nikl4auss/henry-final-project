7f90af199f7e6370f7c043c9853dd2557160e2af
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2022-08-08 12:43:26 -0300

    modified the user administration panel to use the proper function from the userServices file

--- client/src/components/Admin/UserDashboard/UserDashboard.jsx
+++ client/src/components/Admin/UserDashboard/UserDashboard.jsx
@@ -4,18 +4,46 @@ import LoginButton from "../../LoginButton/LoginButton";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUsers } from "../../../redux/actions";
+import { blockUser } from '../../../services/usersServices'
 import styles from "./UserDashboard.module.css";
+import { toast } from "react-toastify";
 
 function UserDashboard() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const usuario = useSelector((state) => state.allUser);
 
+  async function handleBlock(user) {
+    const { id: userId, blocked } = user
+    const token = await getAccessTokenSilently()
+    toast.promise(
+      blockUser(userId, !blocked, token),
+      {
+        pending: {
+          render() {
+            return `${blocked ? 'Desbloqueando' : 'Bloqueando'} Usuario`
+          }
+        },
+        success: {
+          render() {
+            dispatch(getUsers())
+            return `${!blocked === true ? 'Bloqueaste' : 'Desbloqueaste'} a ${user.name}`
+          }
+        },
+        error: {
+          render({ data }) {
+            console.log(data)
+            return `No se pudo ${blocked ? 'Desbloquear' : 'Bloqqquear'} al usuario`
+          }
+        }
+      }
+    )
+  }
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   return (
     <div className={styles.generalContainer}>
@@ -29,7 +57,7 @@ function UserDashboard() {
                   <th className={styles.headCell}>Nombre</th>
                   <th className={styles.headCell}>Apellido</th>
                   <th className={styles.headCell}>email</th>
-                  <th className={styles.headCellBtn}>Cambiar rol</th>
+                  <th className={styles.headCellBtn}>Bloquear Usuario</th>
                   <th className={styles.headCellBtn}>Eliminar usuario</th>
                   <th className={styles.headCellBtn}>Resetear contraseña</th>
                 </tr>
@@ -41,8 +69,8 @@ function UserDashboard() {
                     <td className={styles.cell}>{item.surname}</td>
                     <td className={styles.cell}>{item.email}</td>
                     <td className={styles.btnCell}>
-                      <button onClick={() => alert("Nico! cambiame el rol!!")}>
-                        X
+                      <button onClick={() => handleBlock(item)}>
+                        {`${item.blocked ? 'Desbloquear' : 'Bloquear'}`}
                       </button>
                     </td>
                     <td className={styles.btnCell}>
