0a4266129952a8a2c8e86366d678babc2a7d5d6f
gabrielweht <gabrielestebanw@gmail.com>
2022-08-18 22:17:30 -0300

    Added div and classname

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -6,90 +6,91 @@ import styles from './filters.module.css'
 import { useSessionStorage } from "../../services/useStorage"
 
 
-function Filters () {
+function Filters() {
     let name = useSelector(state => state.name)
     let brands = useSelector(state => state.brands)
     let categories = useSelector(state => state.categories)
     let filtersSelected = useSelector(state => state.filtersSelected)
     let dispatch = useDispatch()
 
-    const [filtersToApply, setFiltersToApply ] = useSessionStorage('filtersSelected', filtersSelected)
-    const [filtersSelectedToRender, setFiltersSelectedToRender ] = useSessionStorage('filter', '')
+    const [filtersToApply, setFiltersToApply] = useSessionStorage('filtersSelected', filtersSelected)
+    const [filtersSelectedToRender, setFiltersSelectedToRender] = useSessionStorage('filter', '')
 
-    
+
     useEffect(() => {
         dispatch(getBrands())
         dispatch(getCategories())
         dispatch(getProducts(filtersToApply, name))
     }, [dispatch, filtersToApply])
 
-    function handleSelectCategory (e) {
+    function handleSelectCategory(e) {
         e.preventDefault()
-        setFiltersToApply({...filtersToApply, 
+        setFiltersToApply({
+            ...filtersToApply,
             category: [...filtersToApply.category, e.target.value]
         })
         setFiltersSelectedToRender([...filtersSelectedToRender, e.target.value])
-        dispatch(addPage( { firstValue: 0, lastValue: 12 }))
+        dispatch(addPage({ firstValue: 0, lastValue: 12 }))
     }
 
     function handleSelectBrand(e) {
         e.preventDefault()
-        setFiltersToApply({...filtersToApply, 
+        setFiltersToApply({
+            ...filtersToApply,
             brand: [...filtersToApply.brand, e.target.value]
         })
         setFiltersSelectedToRender([...filtersSelectedToRender, e.target.value])
-        dispatch(addPage( { firstValue: 0, lastValue: 12 }))
+        dispatch(addPage({ firstValue: 0, lastValue: 12 }))
     }
 
     const handleDeleteFilter = (e) => {
-            e.preventDefault()
-            setFiltersSelectedToRender(filtersSelectedToRender.filter(filter => filter !== e.target.value))
-            if(filtersToApply.category.includes(e.target.value)) {
-                setFiltersToApply({
-                    ...filtersToApply,
-                    category: filtersToApply.category.filter(el => el !== e.target.value)
-                })
-            }
-            if(filtersToApply.brand.includes(e.target.value)) {
-                setFiltersToApply({
-                    ...filtersToApply,
-                    brand: filtersToApply.brand.filter(el => el !== e.target.value)
-                })
-            }
+        e.preventDefault()
+        setFiltersSelectedToRender(filtersSelectedToRender.filter(filter => filter !== e.target.value))
+        if (filtersToApply.category.includes(e.target.value)) {
+            setFiltersToApply({
+                ...filtersToApply,
+                category: filtersToApply.category.filter(el => el !== e.target.value)
+            })
         }
+        if (filtersToApply.brand.includes(e.target.value)) {
+            setFiltersToApply({
+                ...filtersToApply,
+                brand: filtersToApply.brand.filter(el => el !== e.target.value)
+            })
+        }
+    }
 
-    return(
+    return (
         <div className={styles.divFilters}>
-        <div>Filtrar por:</div>
-        <div >
-            <div className={styles.filterSelected}>
-                {filtersSelectedToRender.length > 0 ? 
-                filtersSelectedToRender.map((filter, index) => {
-                    return (
-                        <div className={styles.buttonsSelected}
-                        key={index}>
-                            <button 
-                            value={filter}
-                            onClick={handleDeleteFilter}>{filter} x</button>
-                            <span></span>
-                        </div>
-                    )
-                }) 
-                :
-                <div></div>
-            }
+            <div >
+                <div className={styles.filterSelected}> 
+                    {filtersSelectedToRender.length > 0 ?
+                        filtersSelectedToRender.map((filter, index) => {
+                            return (
+                                <div className={styles.buttonsSelected}
+                                    key={index}>
+                                    <button
+                                        value={filter}
+                                        onClick={handleDeleteFilter}>{filter} x</button>
+                                    <span></span>
+                                </div>
+                            )
+                        })
+                        :
+                        undefined
+                    }
+                </div>
+                <BrandsComponent
+                    handleSelectBrand={handleSelectBrand}
+                    brands={brands}
+                    filtersSelectedToRender={filtersSelectedToRender}
+                />
+                <CategoriesComponent
+                    handleSelectCategory={handleSelectCategory}
+                    categories={categories}
+                    filtersSelectedToRender={filtersSelectedToRender}
+                />
             </div>
-            <BrandsComponent 
-                handleSelectBrand = {handleSelectBrand}
-                brands = {brands}
-                filtersSelectedToRender = {filtersSelectedToRender}
-            />
-            <CategoriesComponent 
-                handleSelectCategory = {handleSelectCategory}
-                categories = {categories}
-                filtersSelectedToRender = {filtersSelectedToRender}
-            />
-        </div>
         </div>
     )
 }
